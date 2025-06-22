7beb9bae8b3cc4526ab278c9820a83a808126cf6
ostsam <ost.sam@gmail.com>
2025-06-21 20:48:22 -0400

    bakery post

--- app/project-list.tsx
+++ app/project-list.tsx
@@ -6,10 +6,10 @@ export type ListItem = {
 };
 
 export const list: ListItem[] = [
-  { project: "Tic-Tac-Toe", link: "https://tictactoe-lime-sigma.vercel.app/" },
+  { project: "Malan Language Tutor", link: "https://malan.vercel.app/" },
   {
     project: "Pet Rock Simulator",
     link: "https://pet-rock-simulator.vercel.app/",
   },
-  { project: "Malan Language Tutor", link: "https://placeholder.com" },
+  { project: "Tic-Tac-Toe", link: "https://tictactoe-lime-sigma.vercel.app/" },
 ];
