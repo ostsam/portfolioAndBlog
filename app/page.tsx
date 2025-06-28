6bb1006a4778ecd9a65740916ee44f98f1bb75c6
ostsam <ost.sam@gmail.com>
2025-06-28 14:27:13 -0400

    carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,38 @@
 import { BlogPosts } from "app/components/posts";
 import { list } from "./project-list";
+import { CarouselDemo } from "./ui/project-carousel";
 
 export default function Page() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <div className="mb-4">
-        <p>{`My name is Sam, and I'm just a wordcel living in the shape rotators' world. 
+    <>
+      {" "}
+      <section>
+        <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+          My Portfolio
+        </h1>
+        <div className="mb-4">
+          <p>{`My name is Sam, and I'm just a wordcel living in the shape rotators' world. 
         Join me on my coding journey and read my thoughts if you're so inclined.`}</p>
-        <br></br>
-        <p>
-          What I'm currently doing:{" "}
-          <a
-            className="hover:animate-ping"
-            href={"https://fractalbootcamp.com/"}
-          >
-            Fractal Bootcamp
-          </a>
-        </p>
-        <br></br>
-        <p>{`What I've shipped:`}</p>
-        <ul className="mt-3">
-          {list.map((item) => (
-            <li key={item.project} className="hover:animate-pulse mb-1">
-              <a href={item.link} target="_blank">
-                {item.project}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
-    </section>
+          <br></br>
+          <p>
+            What I'm currently doing:{" "}
+            <a
+              className="hover:animate-ping underline underline-offset-4"
+              href={"https://fractalbootcamp.com/"}
+            >
+              Fractal Bootcamp
+            </a>
+          </p>
+        </div>
+      </section>
+      <section>
+        <div className="mb-20">
+          <CarouselDemo />
+        </div>
+        <div className="my-8">
+          <BlogPosts />
+        </div>
+      </section>
+    </>
   );
 }
---
+++ app/ui/project-carousel.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { Carousel } from "app/components/carousel";
+export function CarouselDemo() {
+  const slideData = [
+    {
+      title: "Malan Language Tutor",
+      buttonText: "Visit Project",
+      link: "https://malan.vercel.app/",
+      src: "/Malan-Slide.png",
+    },
+    {
+      title: "Pet Rock Simulator",
+      buttonText: "Visit Project",
+      link: "https://pet-rock-simulator.vercel.app/",
+      src: "/Rock-Slide.png",
+    },
+    {
+      title: "Tic-Tac-Toe",
+      buttonText: "Visit Project",
+      link: "https://tictactoe-lime-sigma.vercel.app",
+      src: "/TicTacToe-Slide.png",
+    },
+  ];
+  return (
+    <div className="flex flex-col items-center justify-center py-10">
+      <h1 className="text-4xl hover:animate-pulse font-semibold tracking-tighter">
+        What I've shipped
+      </h1>
+      <div className="relative overflow-hidden w-full h-full mv-20 py-14 px-10">
+        <Carousel slides={slideData} />
+      </div>
+    </div>
+  );
+}
